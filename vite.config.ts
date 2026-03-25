a6860d5c1239127365b564288694c14ab7be31b4
Yasir1986 <yasir.akbar86@hotmail.com>
2026-03-25 12:08:29 +0000

    build issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Vitest configuration
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  },
 });
